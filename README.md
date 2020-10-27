5d67a5bce06b01abc84041f9ccf6cfceeb823071
lijun8637 <1109504235@qq.com>
2020-10-27 17:07:33 +0800

    1.init dateFormat

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# jun-date-format
+
+### jun-date-format
+
+npm install jun-date-format
+
+
+## description 
 my dateFormat
+
+将日期对象格式化为字符串
+
+
+
+
---
+++ index.js
@@ -0,0 +1,49 @@
+module.exports = {
+    printMsg:function(format = 'YYYYMMDDHHmmss', date = new Date()){
+        var o = {
+            "Y+": date.getFullYear(),
+            "M+": date.getMonth() + 1, //月份
+            "D+": date.getDate(), //日
+            "H+": date.getHours(), //小时
+            "m+": date.getMinutes(), //分
+            "s+": date.getSeconds(), //秒
+            "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+            "S": date.getMilliseconds() //毫秒
+        };
+        for (var k in o){
+          if (new RegExp("(" + k + ")").test(format)) {
+            format = format.replace(RegExp.$1, (RegExp.$1.length == 1 || RegExp.$1.length == 4) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)) );
+          }
+        }
+        return format;
+    },
+    format:function(){
+        console.log('hello')
+    },
+    parseDay:function(start,end){
+        var  startDate = Date.parse(start);
+        var  endDate = Date.parse(end);
+        var days = (endDate - startDate)/(1*24*60*60*1000);
+        return  days;
+    },
+    parse:function(start,end){
+        var  startTime = Date.parse(start);
+        var  endTime = Date.parse(end);
+        return endTime - startTime;
+    },
+    //获取月份天数
+    getMonthLength:function(y = new Date().getFullYear(), m = new Date().getMonth()+1, d=1){
+        var myDate= new Date();
+        myDate.setFullYear(y,m-1,d);
+        //设置某个月份的最后一天
+        myDate.setMonth(m,0);
+        var dates = myDate.getDate();
+        return dates;
+    },
+    setDate:function(date,num){
+        date = new Date(date);
+        res = new Date(date.setDate(date.getDate()+num))
+        return res;
+    }
+}
+  
